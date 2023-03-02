e312dafeec9c90d9e484c8c7977efa5e8113b3d0
“ijasz” <jass07rtr@gmail.com>
2023-03-02 11:22:45 +0530

    primitive type, reference type and about window

---
+++ JS/window_object/index.js
@@ -0,0 +1,106 @@
+console.log("hello js");
+console.log("");
+
+// var a = 10;
+// window.b = 20;
+// c = 30;
+
+// console.log(window.a);
+// console.log(a);
+// console.log(b);
+// console.log(window.b);
+// console.log(c);
+
+// function name() {
+//     a = 10;
+//     console.log(a, "inside func");
+// }
+
+// name()
+
+// {
+//     var a = 10;
+// }
+
+
+// console.log(a, "outside func");
+
+
+
+// {
+//     // block scope
+//     var a = 20;
+//     console.log(a);
+// }
+
+
+// reassignable
+
+// var a;
+
+// a = 10;
+// a = "changed";
+
+
+// console.log(typeof a);
+// console.log(a);
+
+
+// redeclarable
+
+// var a = 10;
+// var a = 20;
+
+// console.log(typeof a);
+// console.log(a);
+
+// Primitive type
+
+// var a = 10; //Memory 5654
+// var b = a; //Memory 5645654
+// var c = b; //Memory 56456jkghtf
+
+// b = 20;
+
+// console.log(a, b, c);
+
+// reference type
+
+// var a = [1, 2, 3, 4]; //Memory 5654
+// var b = a; //Memory 5654
+// var c = a; //Memory 5654
+// var d = a; //Memory 5654
+
+// d[0] = "changed";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// var person = {
+//     name: "brathesh",
+//     designation: "developer"
+// }
+
+// var a = person;
+// var b = person;
+// var c = person;
+// var d = person;
+
+// d.designation = "JS developer";
+
+
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.designation);
+// console.log("or");
+// console.log(person["name"]);
+// console.log(person["designation"]);
+
+// console.log(person);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
