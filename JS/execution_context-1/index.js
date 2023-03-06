65bb83d5e5164f0cce28a245c196b4308a2d612a
“ijasz” <jass07rtr@gmail.com>
2023-03-06 11:48:25 +0530

    let&const TDZ Scopes updated in js

---
+++ JS/execution_context-1/index.js
@@ -0,0 +1,12 @@
+// a = 20;
+// b = 50;
+// c = 40;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a;
+// var b;
+// var c;
+
---
+++ JS/let&const/index.js
@@ -0,0 +1,46 @@
+// let a = "outer_scope";
+
+// block scope
+
+// {
+//     let a = "inner_scope"
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+// TDZ
+// console.log(a);
+
+// const a = 10;
+
+// a = 10;
+
+// const a = 20;
+
+// console.log(a);
+
+// {
+//     var a = 100;
+//     console.log(a);
+// }
+
+// const arr = ["a", "b", "c"];
+
+// arr[0] = "changed"
+
+// console.log(arr);
+
+// const obj = {
+//     name: "xxx"
+// };
+
+
+// // obj["name"]
+// obj.name = "changed"
+
+// console.log(obj);
+
+
+
