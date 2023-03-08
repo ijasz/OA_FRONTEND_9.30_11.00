b70bfa07f5c66d29b2c1654ef7fa62647363ea2d
“ijasz” <jass07rtr@gmail.com>
2023-03-08 12:19:21 +0530

    updated

---
+++ JS/conditional_statement/index.js
@@ -0,0 +1,33 @@
+// if (false) {
+//     console.log("if");
+// } else if (false) {
+//     console.log("if");
+// }
+// else if (true) {
+//     console.log("if");
+// }
+// else {
+//     console.log("else");
+// }
+
+// if (false) {
+//     console.log(1);
+// }
+
+// if (true) {
+//     console.log(2);
+// }
+
+// if (false) {
+//     console.log(3);
+// }
+
+// else {
+//     console.log("1 - else");
+// }
+
+const user = "brathesh";
+
+console.log(`hi my self ${user}`);
+console.log("hi my self" + " " + user);
+
